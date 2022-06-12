5594e7ea1105144026e4747ba5a708b955b4c208
Veerendra <veerendranath0312@gmail.com>
2022-06-12 10:58:18 +0530

    Submit part3 exercises 3.1 - 3.6

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,99 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
+];
+
+app.get('/info', (req, res) => {
+  const html = `
+    <p>Phonebook has info for ${persons.length} people</p>
+
+    <p>${new Date()}</p>
+  `;
+  res.send(html);
+});
+
+app.get('/api/persons', (req, res) => {
+  res.status(200).json(persons);
+});
+
+app.post('/api/persons', (req, res) => {
+  const data = req.body;
+
+  const person = persons.find(
+    person => person.name.toLowerCase() === data.name.toLowerCase()
+  );
+
+  // checking if both name and number fields are valid
+  if (!(data.name && data.number)) {
+    return res.status(400).json({
+      error: 'Please provide both name and number'
+    });
+  }
+
+  // checking if the person exists with the provided name
+  if (person) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
+  const newPerson = {
+    name: data.name,
+    number: data.number,
+    id: Math.floor(Math.random() * 100000)
+  };
+
+  persons = persons.concat(newPerson);
+  return res.json(newPerson);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const { id } = req.params;
+  const person = persons.find(person => person.id === Number(id));
+
+  if (!person) {
+    return res.status(404).end();
+  }
+
+  res.status(200).json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const { id } = req.params;
+  const person = persons.find(person => person.id === Number(id));
+
+  if (!person) {
+    return res.status(404).end();
+  }
+
+  persons = persons.filter(person => person.id !== Number(id));
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Listening to server at port ${PORT}`);
+});
