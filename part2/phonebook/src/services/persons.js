6ccecfbc016d60f995561460264c1f2252df2c55
Veerendra <veerendranath0312@gmail.com>
2022-06-23 23:58:18 +0530

    Update baseUrl

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const baseUrl = 'https://phonebookapp-service.herokuapp.com/api/persons';
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = 'https://phonebookapp-service.herokuapp.com/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
