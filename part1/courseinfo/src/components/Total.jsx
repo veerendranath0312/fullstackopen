0c9ae7692a91b4b3e4d0f00f1f95e08d6762a1b0
Veerendranath <veerendranath0312@gmail.com>
2023-04-11 14:24:06 +0530

    Submit exercies 1.3 - 1.5 solutions

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,7 +1,12 @@
 function Total(props) {
   return (
     <div>
-      <p>Number of exercises {props.totalExercises}</p>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
     </div>
   );
 }
