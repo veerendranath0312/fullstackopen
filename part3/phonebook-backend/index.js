c22dc29f88e03378cb00d3577a64457a02b13ba5
Veerendranath <veerendranath0312@gmail.com>
2023-05-03 11:09:40 +0530

    Submit exercises 3.13 - 3.14 solutions

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const connectDB = require("./db/connect.js");
+const Person = require("./models/person.js");
 
 const app = express();
 
@@ -14,39 +17,20 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/info", (req, res) => {
-  const info = `
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `;
-  res.status(200).send(info);
+  Person.find({}).then((people) => {
+    const info = `
+      <p>Phonebook has info for ${people.length} people</p>
+      <p>${new Date()}</p>
+    `;
+    res.status(200).send(info);
+  });
 });
 
 app.get("/api/persons", (req, res) => {
-  res.status(200).json(persons);
+  Person.find({}).then((people) => {
+    res.status(200).json(people);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
@@ -76,23 +60,32 @@ app.post("/api/persons", (req, res) => {
     return res.status(400).json({ error: "name or number missing" });
   }
 
-  const person = persons.find((person) => person.name === data.name);
-  if (person) {
-    return res.status(400).json({ error: "name must be unique" });
-  }
+  // const person = persons.find((person) => person.name === data.name);
+  // if (person) {
+  //   return res.status(400).json({ error: "name must be unique" });
+  // }
 
-  const newPerson = {
-    id: Math.floor(Math.random() * 100000),
+  const newPerson = Person({
     name: data.name,
     number: data.number,
-  };
+  });
 
-  persons = [...persons, newPerson];
-  res.status(201).json(newPerson);
+  newPerson.save().then((savedPerson) => {
+    res.status(201).json(savedPerson);
+  });
 });
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}/`);
+    });
+  } catch (error) {
+    console.log("Error: ", error.message);
+  }
+};
+
+start();
